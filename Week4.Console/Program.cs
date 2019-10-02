ccd5ebf39e71ef225088bd6d00c3093e7f0cb882
Samuel Colville <S00182360@mail.itsligo.ie>
2019-10-02 15:57:42 +0100

    added generic

--- Week4.Console/Program.cs
+++ Week4.Console/Program.cs
@@ -14,28 +14,50 @@ namespace Week4.Console
         static void Main(string[] args)
         {
             List<Programme> programmes = new List<Programme>();
-                // Get the current assembly
-                Assembly assembly = Assembly.GetExecutingAssembly();
-                // Assembly name and resource stored in assembly
-                string resourceName = "Week4.Console.Courses.csv";
-                // Get the embedded resource from the assembly
-                using (Stream stream = assembly.GetManifestResourceStream(resourceName))
-                {   // create a stream reader
-                    using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+            // Get the current assembly
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            // Assembly name and resource stored in assembly
+            string resourceName = "Week4.Console.Courses.csv";
+            // Get the embedded resource from the assembly
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {   // create a stream reader
+                using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+                {
+                    // create a csv reader dor the stream
+                    CsvReader csvReader = new CsvReader(reader);
+                    csvReader.Configuration.HasHeaderRecord = false;
+                    programmes = csvReader.GetRecords<Programme>().ToList();
+                    // Read the records into the desired collection of that type
+                    // and iterate over the collection
+                    foreach (var item in programmes)
                     {
-                        // create a csv reader dor the stream
-                        CsvReader csvReader = new CsvReader(reader);
-                        csvReader.Configuration.HasHeaderRecord = false;
-                        programmes = csvReader.GetRecords<Programme>().ToList();
-                        // Read the records into the desired collection of that type
-                        // and iterate over the collection
-                        foreach (var item in programmes)
-                        {
-                            System.Console.WriteLine("{0}", item.ToString());
-                        }
-                        System.Console.ReadKey();
+                        System.Console.WriteLine("{0}", item.ToString());
                     }
                 }
+            }
+            resourceName = "Week4.Console.Student.csv";
+            List<Student> students = Get<Student>(resourceName);
+            foreach (var stud in students)
+            {
+                System.Console.WriteLine(stud.ToString());
+            }
+
+            System.Console.ReadKey();
+
+        }
+
+        public static List<T> Get<T>(string resourceName)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using(StreamReader sr = new StreamReader(stream, Encoding.UTF8))
+                {
+                    CsvReader csvReader = new CsvReader(sr);
+                    csvReader.Configuration.HasHeaderRecord = false;
+                    return csvReader.GetRecords<T>().ToList();
+                }
+            }
         }
     }
 }
---
+++ Week4.Console/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4.Console
+{
+    class Student
+    {
+        public string StudentNumber { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+
+        public override string ToString()
+        {
+            return FirstName + " " + Surname + "\tStudent Number: " + StudentNumber;
+        }
+    }
+}
